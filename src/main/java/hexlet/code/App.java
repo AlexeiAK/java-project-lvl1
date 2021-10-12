8f1b387d9c23a17b6a585f3b918cfc3b4908f2b9
AlexeiAK <aak3xx@gmail.com>
2021-10-12 15:19:40 +0300

    feat: user enter his name in the console add Cli.java; add Makefile; modified gitignore(.idea);

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,12 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Welcome to the Brain Games!\n"
+        + "May I have your name?");
+
+        Cli.enterName();
+
+        System.out.println("Hello, " +
+                Cli.enterName().nextLine() + "!");
     }
 }
---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+import java.util.Scanner;
+
+class Cli {
+    static Scanner enterName() {
+        Scanner name = new Scanner(System.in);
+        return name;
+    }
+}
