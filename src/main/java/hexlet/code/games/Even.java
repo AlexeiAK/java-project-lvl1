1eeb536c514bc9e8623184ae6b205a31a2911676
AlexeiAK <aak3xx@gmail.com>
2022-02-01 15:53:36 +0300

    refactor: redesign relations of games and Engine: only Even

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -4,23 +4,32 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Even {
-    static final String GAME_RULES = "Answer 'yes' if number even otherwise answer 'no'.";
+    static final String GAME_RULES = "Answer 'yes' if number even. Otherwise answer 'no'.";
 
     public static void startGame() {
-        String[] question = new String[Engine.MAX_ROUNDS];
-        String[] answer = new String[Engine.MAX_ROUNDS];
-
         String[][] questionAndAnswer = new String[Engine.MAX_ROUNDS][1];
 
         for (int i = 0; i < Engine.MAX_ROUNDS; i++) {
-            question[i] = Integer.toString(Utils.getNumberFromFinalRange());
-            answer[i] = isNumberEven(Integer.parseInt(question[i]));
+            questionAndAnswer[i] = getNumberWithAnswer();
         }
 
         Engine.startGame(GAME_RULES, questionAndAnswer);
     }
 
-    public static String isNumberEven(int number) {
-        return (number % 2 == 0) ? "yes" : "no";
+    public static String[] getNumberWithAnswer() {
+        int randomNumber = Utils.getNumberFromFinalRange();
+
+        String question = Integer.toString(randomNumber);
+        String answer = isNumberEven(randomNumber) ? "yes" : "no";
+
+        String[] numberWithAnswer = new String[2];
+        numberWithAnswer[0] = question;
+        numberWithAnswer[1] = answer;
+
+        return numberWithAnswer;
+    }
+
+    public static boolean isNumberEven(int number) {
+        return number % 2 == 0;
     }
 }
