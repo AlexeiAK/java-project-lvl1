ae14df1a2ba6264a5dd6df81d7a75c6c34dd01c8
AlexeiAK <aak3xx@gmail.com>
2022-02-08 18:38:08 +0300

    refactor: purge excess method in Calc

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -3,12 +3,8 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-import java.util.Random;
-
 public class Calc {
     static final String GAME_RULES = "What is the result of the expression?";
-
-    static final int NUMBER_OF_TYPES_MATH_OPERATIONS = 3;
 
     static final int MAX_OF_RANDOM_RANGE = 1;
     static final int MIN_OF_RANDOM_RANGE = 10;
@@ -21,32 +17,26 @@ public class Calc {
             int operandB = Utils.getRandomInRange(MAX_OF_RANDOM_RANGE, MIN_OF_RANDOM_RANGE);
             String operator = getRandomOperationSign();
 
-            questionsAndAnswers[i] = getRandomQuestionAndAnswer(operandA, operandB, operator);
+            questionsAndAnswers[i] = new String[]{
+                    operandA + " " + operator + " " + operandB,
+                    calculate(operator, operandA, operandB)};
         }
 
         Engine.startGame(GAME_RULES, questionsAndAnswers);
     }
 
-    public static String[] getRandomQuestionAndAnswer(int operandA, int operandB, String operator) {
-        String a = Integer.toString(operandA);
-        String b = Integer.toString(operandB);
-
-        String question = a + " " + operator + " " + b;
-
-        String answer = switch (operator) {
+    public static String calculate(String operator, int operandA, int operandB) {
+        return switch (operator) {
             case "+" -> Integer.toString(operandA + operandB);
             case "-" -> Integer.toString(operandA - operandB);
             case "*" -> Integer.toString(operandA * operandB);
             default -> "Wrong operator!";
         };
-
-        return Utils.pairOf(question, answer);
     }
 
     public static String getRandomOperationSign() {
         String[] totalOperations = {"+", "-", "*"};
-        Random randomOperation = new Random();
 
-        return totalOperations[randomOperation.nextInt(NUMBER_OF_TYPES_MATH_OPERATIONS)];
+        return totalOperations[Utils.getRandomToMax(totalOperations.length - 1)];
     }
 }
