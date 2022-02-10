0ca2c6566ebba7d741e4ecedc58143dfeb850fae
AlexeiAK <aak3xx@gmail.com>
2022-02-10 11:31:53 +0300

    refactor: more simplicity in Engine

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -15,24 +15,30 @@ public class Engine {
         System.out.println("Hello, " + playerName + "!");
         System.out.println(gameRules);
 
+        int roundsCounter = 0;
         Scanner answerScanner = new Scanner(System.in);
 
-        for (int i = 0; i < MAX_ROUNDS; i++) {
-            System.out.println("Question: " + questionsAndAnswers[i][0]);
+        for (String[] questionAndAnswer : questionsAndAnswers) {
+            String question = questionAndAnswer[0];
+            String answer = questionAndAnswer[1];
+
+            System.out.println("Question: " + question);
             System.out.print("Your answer: ");
             String playerAnswer = answerScanner.next();
 
-            if (questionsAndAnswers[i][1].equals(playerAnswer)) {
+            if (answer.equals(playerAnswer)) {
                 System.out.println("Correct!");
+                roundsCounter++;
             } else {
                 System.out.print("'" + playerAnswer + "' is wrong answer ;(. Correct answer was '"
-                        + questionsAndAnswers[i][1] + "'.\n"
+                        + answer + "'.\n"
                         + "Let's try again, " + playerName + "!\n");
                 return;
             }
         }
 
-        System.out.println("Congratulations, " + playerName + "!");
+        if (roundsCounter == MAX_ROUNDS) {
+            System.out.println("Congratulations, " + playerName + "!");
+        }
     }
-
 }
